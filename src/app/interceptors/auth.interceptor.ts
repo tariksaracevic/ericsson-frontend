e7b8357d6f4d27f246211fb981de86a43c0b0fd8
SaracevicTarik <saracevic998@gmail.com>
2024-11-05 00:40:41 +0100

    Auth front

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token'); // Retrieve the token
+
+  // Clone the request to add the Authorization header
+  if (token) {
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`)
+    });
+    return next(authReq);
+  }
+
+  // If no token, forward the original request
+  return next(req);
+};
