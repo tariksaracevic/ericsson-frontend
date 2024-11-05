c9f7af0c4fbe324c930694bcda32bef82f84a812
SaracevicTarik <saracevic998@gmail.com>
2024-11-05 19:33:54 +0100

    Styling

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatCard} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
